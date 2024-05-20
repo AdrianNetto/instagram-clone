a4b96df5a4c15b9767cd5a7374ce2f12b559f491
AdrianNetto <adrian.netto73@gmil.com>
2024-05-20 14:12:55 -0300

    feat: adding mini profile component

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+import React from "react";
 
 export default function MiniProfile() {
+  const { data: session } = useSession();
+
   return (
-    <div>MiniProfile</div>
-  )
+    <div className="flex items-center justify-between mt-14 scroll-ml-10">
+      <img
+        src={
+          session?.user?.image ||
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
+        }
+        className="w-16 h-16 rounded-full border p-[2px]"
+        alt="user profile pic or instagram logo"
+      />
+      <div className="flex-1 ml-4">
+        <h2 className="font-bold">{session?.user?.username}</h2>
+        <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
+      </div>
+      {
+        session ? (
+          <button onClick={signOut} className="text-blue-500 text-sm font-semibold">
+            Sign out
+          </button>
+        ) : (
+          <button onClick={signIn} className="text-blue-500 text-sm font-semibold">
+            Sign in
+          </button>
+        )
+      }
+    </div>
+  );
 }
